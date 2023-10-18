bd418a2ba72afd5efc5c3bac2779a9d47c7f41cd
Varun Madiath <git@madiathv.com>
2023-10-18 17:44:24 -0400

    Add nix devshell and envrc

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (system: {
+      default = pkgs.${system}.mkShellNoCC {
+        packages = with pkgs.${system}; let
+          pythonEnv = python3.withPackages (
+            ps:
+              with ps; [
+                requests
+                jsonschema
+                psutil
+                python-pidfile
+              ]
+          );
+        in [
+          pythonEnv
+        ];
+      };
+    });
+  };
+}
